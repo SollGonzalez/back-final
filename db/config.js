49ad840a931ea254166797c17b92808750f343fc
solgonzalez <gonzalezsolailen@gmail.com>
2022-07-31 19:03:44 -0300

    act3

--- db/config.js
+++ db/config.js
@@ -1,26 +1,3 @@
-// const mysql = require("mysql");
-// const util = require("util");
-// //DB CONNECITON MYSQL WORKBENCH
-// const db = mysql.createPool({
-//   host: process.env.db_host,
-//   user: process.env.db_user,
-//   database: process.env.db_name,
-//   password: process.env.pass,
-// });
-
-// db.getConnection((err, connection) => {
-//   if (err)
-//     console.warn("Unable to connect to database", { Error: err.message });
-//   else {
-//     console.dir("Connection to database established");
-//     db.releaseConnection(connection);
-//   }
-// });
-
-// db.query = util.promisify(db.query);
-
-// module.exports = db;
-
 const mysql = require("mysql");
 const util = require("util");
 const connection = mysql.createPool({

9d945c7c536064e106c5c597d91d7398ff3fb333
solgonzalez <gonzalezsolailen@gmail.com>
2022-07-31 19:00:38 -0300

    act 2

--- db/config.js
+++ db/config.js
@@ -1,22 +1,38 @@
+// const mysql = require("mysql");
+// const util = require("util");
+// //DB CONNECITON MYSQL WORKBENCH
+// const db = mysql.createPool({
+//   host: process.env.db_host,
+//   user: process.env.db_user,
+//   database: process.env.db_name,
+//   password: process.env.pass,
+// });
+
+// db.getConnection((err, connection) => {
+//   if (err)
+//     console.warn("Unable to connect to database", { Error: err.message });
+//   else {
+//     console.dir("Connection to database established");
+//     db.releaseConnection(connection);
+//   }
+// });
+
+// db.query = util.promisify(db.query);
+
+// module.exports = db;
+
 const mysql = require("mysql");
 const util = require("util");
-//DB CONNECITON MYSQL WORKBENCH
-const db = mysql.createPool({
+const connection = mysql.createPool({
   host: process.env.db_host,
-  user: process.env.db_user,
   database: process.env.db_name,
-  password: process.env.pass,
+  user: process.env.db_user,
+  password: process.env.db_pass,
 });
-
-db.getConnection((err, connection) => {
-  if (err)
-    console.warn("Unable to connect to database", { Error: err.message });
-  else {
-    console.dir("Connection to database established");
-    db.releaseConnection(connection);
-  }
+connection.getConnection((err) => {
+  err
+    ? console.log("No conectado", { Error: err.message })
+    : console.log("Conexion establecida..");
 });
-
-db.query = util.promisify(db.query);
-
-module.exports = db;
+connection.query = util.promisify(connection.query);
+module.exports = connection;
